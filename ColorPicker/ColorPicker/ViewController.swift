e8e4a9ee7194949b770859906afd4c5f7a3da161
Chaz <c.bruscato97@gmail.com>
2018-10-05 14:07:34 -0500

    Created project

---
+++ ColorPicker/ColorPicker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ColorPicker
+//
+//  Created by Chaz on 10/5/18.
+//  Copyright © 2018 Chaz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
